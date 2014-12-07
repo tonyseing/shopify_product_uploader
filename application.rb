a65507d7afaf1e88c2a76671cebad1ced7339544
Tony Seing <tony.seing@gmail.com>
2014-12-07 02:11:20 -0500

    uploader presentation ready. upload works, with user accoutns

--- application.rb
+++ application.rb
@@ -3,15 +3,72 @@ require 'sinatra/flash'
 require 'pry'
 require 'shopify_api'
 require 'base64'
+require 'firebase'
 
 enable :sessions
+use Rack::Session::Cookie, :key => 'rack.session',
+:path => '/',
+:expire_after => 2592000, # In seconds
+:secret => 'makesuretochangeme!'
+
 
 API_KEY = ENV["API_KEY"]
 PASSWORD = ENV["PASSWORD"]
 SHOP_NAME = ENV["SHOP_NAME"] 
 
+# shop config
 shop_url = "https://#{API_KEY}:#{PASSWORD}@#{SHOP_NAME}.myshopify.com/admin"
 ShopifyAPI::Base.site = shop_url
+
+# firebase config
+base_uri = 'https://palette3.firebaseio.com/'
+firebase = Firebase::Client.new(base_uri)
+
+
+get '/signup' do
+  erb :"signup.html"
+end
+
+get '/signout' do
+  session['user'] = nil
+  redirect '/'
+end
+
+post '/signup' do
+  new_user = params["user"]
+  response = firebase.push("users", { :email => new_user["email"], :password => Digest::SHA1.hexdigest(new_user["password"]), :name => new_user["name"] })
+  if response.success?
+    session['user'] = new_user["email"]
+    redirect '/upload'
+  else
+    redirect '/signup'
+  end
+end
+
+get '/signin' do
+  erb :"signin.html"
+end
+
+                 
+post '/signin' do
+  user = params["user"]
+  db_users = firebase.get("users").body.map { |k, v| v }
+
+  maybe_user = db_users.select { |artist| (artist["email"] == user["email"])  and (artist["password"] == Digest::SHA1.hexdigest(user["password"]))  }
+
+  if maybe_user.length >= 1
+
+    session['user'] = maybe_user[0]["email"]
+    redirect '/upload'
+  else
+    redirect '/signin'
+  end
+end
+
+
+get '/' do
+  
+end
 
 get '/upload' do
   erb :"upload.html"
@@ -22,13 +79,16 @@ post '/upload' do
   product = params["product"]
   images =[]
   product["images"].each_with_index do |image|
-    images.push({ attachment:  Base64.encode64(open(image[:tempfile]) { |io| io.read })})
+
+    #images.push({ attachment:  Base64.encode64(open(image[:tempfile]) { |io| io.read })})
+    images.push({ attachment:  Base64.encode64(image[:tempfile].read) })
   end
 
   # Create a new product
   new_product = ShopifyAPI::Product.new
+  
+  new_product.title, new_product.body_html, new_product.product_type, new_product.vendor, new_product.images, new_product.variants = [product["title"], product["description"], product["product_type"], session['user'], images, { "price" => 29.99 }]
 
-  new_product.title, new_product.body_html, new_product.product_type, new_product.vendor, new_product.images = [product["title"], product["description"], product["product_type"], product["vendor"], images]
   new_product.save
 
   
